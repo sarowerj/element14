863dd070b72e2f0bb7920c0c00fbc30ff44f3cd9
sarowerj <sarowerj@gmail.com>
2016-09-27 18:25:12 +0600

    Upload Project

---
+++ index.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- Title of Site-->
+        <title>Element 14</title>
+
+        <!-- Bootstrap -->
+        <link href="bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+
+        <!-- Open Sans Font from Google -->
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800" rel="stylesheet">
+
+        <!-- Stylesheet for Theme styling -->
+        <link rel="stylesheet" href="css/style.css" />
+
+        <!--[if lt IE 9]>
+          <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+          <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
+    <body>
+        <nav class="navbar navbar-default">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#main_nav" aria-expanded="false">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="index.php"><img src="images/main_logo.png" class="img-responsive" alt="" /></a>
+                </div>
+
+                <!-- Right Link -->
+                <div class="collapse navbar-collapse" id="main_nav">
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="#">THE Destination for Engineering Software</a></li>
+                    </ul>
+                </div><!-- /.navbar-collapse -->
+            </div><!-- /.container -->
+        </nav>
+
+        <main class="main_blue_bg">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xs-12 col-md-7">
+                        <h1 class="main_title">
+                            <small>Free Download of</small> <br />
+                            Cadsoft 
+                            <span class="text-upper">Eagle</span> 
+                            Express
+                        </h1>
+                        <p>EAGLE, the Easy Applicable Graphical Layout Editor is a powerful PCB design software tailored to meet the needs of professional engineers, makers and students! For more than 25 years, EAGLE has been the PCB design tool of choice for hundreds of thousands of electronic design engineers and developers worldwide. </p>
+                        <h3>Eagle Express includes:</h3>
+                        <div class="row">
+                            <div class="col-md-9">
+                                <ul class="include_list">
+                                    <li>Schematic Editor</li>
+                                    <li>Layout Editor </li>
+                                    <li>2 Signal Layers</li>
+                                    <li>Autorouter</li>
+                                    <li>100 x 80 mm Routing Area </li>
+                                    <li>Schematic Sheets </li>
+                                    <li>Non-Commercial </li>
+                                </ul>
+                            </div>
+                            <div class="col-md-3">
+                                <div class="cad_logo_cont">
+                                    <img src="images/cadsoft_logo.png" class="img-responsive" alt="" />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-4 col-md-offset-1">
+                        <div class="download_box">
+                            <h3>Request Free Download</h3>
+                            <div class="box_content">
+                                <form>
+                                    <div class="form-group">
+                                        <label for="exampleInputEmail1">Email</label>
+                                        <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Jhon@mail.com">
+                                    </div>
+                                    <div class="checkbox">
+                                        <label>
+                                            <input type="checkbox"> I accept - by accepting, I agree to be contacted by Premier Farnell group of companies by email or phone for further information.
+                                        </label>
+                                    </div>
+                                    <p class="hints">For more information about the Premier Farrell privacy policy, visit: www.farnell.com/privacy </p>
+                                    <button type="submit" class="btn btn-default">Submit</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
+
+        <footer>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6 col-xs-12">
+                        <div class="footer_logo">
+                            <img src="images/footer_logo.png" class="img-responsive" alt="" />
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-xs-12">
+                        <div class="footer_text text-right">
+                            A Premier Farnell Company <br />
+                            2016 Premier Farnell plc. All Rights Reserved
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+
+        <!-- jQuery v3.1.1 -->
+        <script src="bower_components/jquery/dist/jquery.min.js"></script>
+
+        <!-- Bootstrap v3.3.7 JS -->
+        <script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+    </body>
+</html>
